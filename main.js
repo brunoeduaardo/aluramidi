fd69dbe717c4ca63e2e12c1c2e76cf101cb7c01d
Bruno Eduardo <brumo1234@gmail.com>
2022-06-17 19:51:47 -0300

    atualização progresso

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 document.querySelector('.tecla_pom');
+document.querySelector('.tecla_clap');
+
+function tocarPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocarClap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+
