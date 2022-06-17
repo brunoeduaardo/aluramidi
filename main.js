3f7c353716fd2a715d43f881edb6c30501e8030f
Bruno Eduardo <brumo1234@gmail.com>
2022-06-17 20:44:34 -0300

    atualização progresso001

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-document.querySelector('.tecla_pom');
-document.querySelector('.tecla_clap');
+
+
+
+document.querySelectorAll('.tecla');
+
+
+
+
+/*
+    Código para cada botão. No caso de um código que terá muitos botões parecidos, ficaria inviável
 
 function tocarPom(){
-    document.querySelector('#som_tecla_pom').play();
+        document.querySelector('#som_tecla_pom').play();
 }
 
-function tocarClap(){
-    document.querySelector('#som_tecla_clap').play();
-}
-
+document.querySelector('.tecla_pom').onclick = tocarPom; 
+*/
