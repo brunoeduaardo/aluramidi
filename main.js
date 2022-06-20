ac22a0a2dffe81c66b195f4cff31343f355bd373
Bruno Eduardo <brumo1234@gmail.com>
2022-06-20 16:16:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 function tocar(seletorAudio){
     const elemento = document.querySelector(seletorAudio);
 
-    if(elemento === null){
-        console.log('Elemento não encontrado!')
+    if(elemento && elemento.localName === 'audio'){ // apenas colocando a constante, o jS já identifica se o elemento é nulo ou não. Descartando a necessidade de usar a verificação "elemento != null"
+        elemento.play();
     } else {
-        if(elemento.localName === 'audio'){
-            elemento.play();
-        };
+        console.log('Elemento não encontrado/Seletor inválido');
     };
 }
 
@@ -16,30 +14,20 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 for(let contador = 0; contador < listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador];
-
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`; // template string
 
     tecla.onclick = function(){
         tocar(idAudio);
-    };
-
-   
+    };  
 
     tecla.onkeydown =  function(evento){
-
         if(evento.code === 'Space' || evento.code === 'Enter'){
             tecla.classList.add('ativa');
         }
-        
-
     };
 
     tecla.onkeyup = function(){
-
         tecla.classList.remove('ativa');
-
-    }
-
-}
+    };
+};
